5734bbf427a8ebaebb5ad6749d8ccecac97fb8f3
Andrey Azov <andrey.azov@gmail.com>
2020-03-22 22:55:05 +0000

    Update lookup on index.json

--- scripts/searchIndex.js
+++ scripts/searchIndex.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const lunr = require('lunr');
 
-const config = require('../config');
-
-const pathToIndex = path.resolve(config.indexOutputPath, config.indexName);
+const pathToIndex = path.resolve(__dirname, '../build/indices/index.json');
 const index = require(pathToIndex);
+
+// const config = require('../config');
+// const pathToIndex = path.resolve(config.indexOutputPath, config.indexName);
+// const index = require(pathToIndex);
 
 const idx = lunr.Index.load(index);
 
