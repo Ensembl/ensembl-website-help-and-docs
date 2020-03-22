90eb048333ab8fd81fb32cfa155c77ee130310e0
Andrey Azov <andrey.azov@gmail.com>
2020-03-22 22:38:21 +0000

    Try having build directory?

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  indexOutputPath: path.resolve(__dirname, 'public/indices'),
+  indexOutputPath: path.resolve(__dirname, 'build/indices'),
   indexName: 'index.json'
 }
