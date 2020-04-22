a4ce6e48537fcd7315beac3363b309b1a21d388d
Andrey Azov <andrey@ebi.ac.uk>
2020-04-22 11:26:00 +0100

    Serve images locally

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
 const getArticle = require('../api/article');
 const search = require('../api/search');
+
+app.use('/images', express.static(path.join(__dirname, '../images')));
 
 app.use('/api/article', getArticle);
 app.use('/api/search', search);
