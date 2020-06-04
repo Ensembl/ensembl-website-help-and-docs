7e1df2b3adc6ad1dee81cabbacf2ac7a39fca755
Andrey Azov <andrey@ebi.ac.uk>
2020-06-04 16:07:26 +0100

    Add script for copying files to build directory

--- build.js
+++ build.js
@@ -1,7 +1,9 @@
 const buildIndex = require('./scripts/build-index');
 const buildDatabase = require('./scripts/build-database');
+const copyAssets = require('./scripts/copy-assets');
 
 (async () => {
-  await buildDatabase();
+  // await buildDatabase();
+  await copyAssets();
   // await buildIndex();
 })();
--- config.js
+++ config.js
@@ -1,12 +1,16 @@
 const path = require('path');
 
 const docsPath = path.resolve(__dirname, 'docs');
-const databaseDirectory = path.resolve(__dirname, 'build');
+const databaseDirectory = path.resolve(__dirname, 'build'); // FIXME?
 const databaseName = 'database.db';
 const indexDirectory = path.resolve(__dirname, 'build/indices');
 const articlesIndexName = 'articlesIndex.json';
+const buildPath = path.resolve(__dirname, 'build');
+const buildImagesPath = path.resolve(buildPath, 'images');
 
 module.exports = {
+  buildPath,
+  buildImagesPath,
   docsPath,
   articlesPath: path.join(docsPath, 'article'),
   videosPath: path.join(docsPath, 'video'),
