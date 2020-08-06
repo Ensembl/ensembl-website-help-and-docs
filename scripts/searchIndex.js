9826eb7bfbf20b07a1a84d8f92958afbe1748c47
Andrey Azov <andrey@ebi.ac.uk>
2020-08-06 18:19:25 +0100

    Remove the old js scripts

--- scripts/searchIndex.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const lunr = require('lunr');
-
-const searchIndex = (query, index) => {
-  const idx = lunr.Index.load(index);
-  return idx.search(query);
-};
-
-module.exports = searchIndex;
