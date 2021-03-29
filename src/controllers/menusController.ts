a3460c31998299328673989f46bb48776c6d3305
Andrey Azov <andrey@ebi.ac.uk>
2021-03-29 11:53:54 +0100

    Rename field in menus payload

--- src/controllers/menusController.ts
+++ src/controllers/menusController.ts
@@ -18,7 +18,7 @@ export const getMenu = async (req: Request, res: Response) => {
     if (menu) {
       res.json({
         name: menu.name,
-        data: menu.data,
+        items: menu.data,
       });
     } else {
       res.status(404);
